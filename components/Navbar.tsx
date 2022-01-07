e8e26b6d369293c462c5a84679ff1963f36e0588
hasnul <hasnul.rsusu@gmail.com>
2022-01-07 15:54:21 +0700

    daftar template progress

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Menu } from 'antd';
 import {
   UnorderedListOutlined,
@@ -13,50 +13,46 @@ import {
 
 const { SubMenu } = Menu;
 
-class Navbar extends React.Component {
-  state = {
-    current: 'rka',
+const Navbar = (props: any) => {
+  const [current, setCurrent] = useState('rka');
+  const handleClick = (e: any) => {
+    setCurrent(e.key);
   };
 
-  handleClick = (e: any) => {
-    console.log('click ', e);
-    this.setState({ current: e.key });
-  };
+  useEffect(() => {
+    const { state } = props;
+    if (state) {
+      setCurrent(state);
+    }
+  }, [props]);
 
-  render() {
-    const { current } = this.state;
-    return (
-      <Menu
-        onClick={this.handleClick}
-        selectedKeys={[current]}
-        mode="horizontal"
-      >
-        <Menu.Item key="rka" icon={<UnorderedListOutlined />}>
-          RKA
-        </Menu.Item>
-        <SubMenu key="SubMenu" icon={<BarChartOutlined />} title="Kinerja">
-          <Menu.Item key="kinerja:2022">RKA 2022</Menu.Item>
-          <Menu.Item key="kinerja:2021S">RKA Suplemen 2021</Menu.Item>
-          <Menu.Item key="kinerja:2021">RKA 2021</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="dpa" icon={<CalendarOutlined />}>
-          DPA
-        </Menu.Item>
-        <Menu.Item key="monev" icon={<EyeOutlined />}>
-          Monev
-        </Menu.Item>
-        <Menu.Item key="template" icon={<CopyOutlined />}>
-          Template
-        </Menu.Item>
-        <Menu.Item key="pengaturan" icon={<SettingOutlined />}>
-          Pengaturan
-        </Menu.Item>
-        <Menu.Item key="tutorial" icon={<InfoCircleFilled />}>
-          Tutorial
-        </Menu.Item>
-      </Menu>
-    );
-  }
-}
+  return (
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Menu.Item key="rka" icon={<UnorderedListOutlined />}>
+        RKA
+      </Menu.Item>
+      <SubMenu key="SubMenu" icon={<BarChartOutlined />} title="Kinerja">
+        <Menu.Item key="kinerja:2022">RKA 2022</Menu.Item>
+        <Menu.Item key="kinerja:2021S">RKA Suplemen 2021</Menu.Item>
+        <Menu.Item key="kinerja:2021">RKA 2021</Menu.Item>
+      </SubMenu>
+      <Menu.Item key="dpa" icon={<CalendarOutlined />}>
+        DPA
+      </Menu.Item>
+      <Menu.Item key="monev" icon={<EyeOutlined />}>
+        Monev
+      </Menu.Item>
+      <Menu.Item key="template" icon={<CopyOutlined />}>
+        Template
+      </Menu.Item>
+      <Menu.Item key="pengaturan" icon={<SettingOutlined />}>
+        Pengaturan
+      </Menu.Item>
+      <Menu.Item key="tutorial" icon={<InfoCircleFilled />}>
+        Tutorial
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 export default Navbar;
